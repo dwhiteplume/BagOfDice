a15ad23fe59586b2b078458a1cd7f7eb17d97b07
Darius Whiteplume <dariuswhiteplume@gmail.com>
2023-07-09 13:49:50 -0400

    initial
    tested successfully in pwsh 7

--- BagOfDice.psm1
+++ BagOfDice.psm1
@@ -4,8 +4,8 @@
 . $(Join-Path $(Join-Path $PSScriptRoot -ChildPath lib) -ChildPath GetBoDFudge.ps1)
 . $(Join-Path $(Join-Path $PSScriptRoot -ChildPath lib) -ChildPath GetBoDPercentileRoll.ps1)
 
-# Export-ModuleMember -Function Get-BoDCoinFlip
-# Export-ModuleMember -Function Get-BoDDiceRolls
-# Export-ModuleMember -Function Get-BoDDieRoll
-# Export-ModuleMember -Function Get-BoDFudge
-# Export-ModuleMember -Function Get-BoDPercentileRoll
+Export-ModuleMember -Function Get-BoDCoinFlip
+Export-ModuleMember -Function Get-BoDDiceRolls
+Export-ModuleMember -Function Get-BoDDieRoll
+Export-ModuleMember -Function Get-BoDFudge
+Export-ModuleMember -Function Get-BoDPercentileRoll
