bac38059cccbca2cf1a1b80d713a319e3b2fca50
Darius Whiteplume <dariuswhiteplume@gmail.com>
2023-07-08 22:14:14 -0400

    initial
    need to change to psm1. weirdness happening, there, so deferring. open an issue.

---
+++ BagOfDice.ps1
@@ -0,0 +1,11 @@
+. $(Join-Path $(Join-Path $PSScriptRoot -ChildPath lib) -ChildPath GetBoDCoinFlip.ps1 -Verbose)
+. $(Join-Path $(Join-Path $PSScriptRoot -ChildPath lib) -ChildPath GetBoDDiceRolls.ps1)
+. $(Join-Path $(Join-Path $PSScriptRoot -ChildPath lib) -ChildPath GetBoDDieRoll.ps1)
+. $(Join-Path $(Join-Path $PSScriptRoot -ChildPath lib) -ChildPath GetBoDFudge.ps1)
+. $(Join-Path $(Join-Path $PSScriptRoot -ChildPath lib) -ChildPath GetBoDPercentileRoll.ps1)
+
+# Export-ModuleMember -Function Get-BoDCoinFlip
+# Export-ModuleMember -Function Get-BoDDiceRolls
+# Export-ModuleMember -Function Get-BoDDieRoll
+# Export-ModuleMember -Function Get-BoDFudge
+# Export-ModuleMember -Function Get-BoDPercentileRoll
