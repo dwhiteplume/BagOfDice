7d78763d6ef9abb883b395b81e17d7efbccd5ac6
Darius Whiteplume <dariuswhiteplume@gmail.com>
2024-01-05 17:02:08 -0500

    create methods
    add method:
    * DieRoll - rolls a single die
    * DiceRolls - rolls multiples of a die type
    * FudgeDie - rolls a single Fuge die (-1,0,1)
    * FudgeRoll - rolls a number of Fudge dice
    * Percentile - rolls 2d10 to obtain a 0-99 result
    * CoinFlip - heads or tails

---
+++ BagOfDice.ps1
@@ -0,0 +1,118 @@
+$BagOfDice = [pscustomobject]@{}
+
+$BagOfDice | Add-Member `
+    -MemberType ScriptMethod `
+    -Name DieRoll `
+    -Value {
+        [CmdletBinding()]
+        param(
+            [int32]$Sides
+        )
+        $Die = 1..$Sides
+        $Roll = $($Die | Get-Random)
+        Write-Output $Roll
+    }
+
+$BagOfDice | Add-Member `
+    -MemberType ScriptMethod `
+    -Name DiceRolls `
+    -Value {
+        [CmdletBinding()]
+        param(
+            [ValidateScript({
+                if ($PSItem -gt 0) {
+                    $true
+                } else {
+                    throw "Value must be greater than zero."
+                }
+            })]
+            [Parameter(Position = 0, Mandatory = $true)]
+            [int32]$Dice,
+            [ValidateScript({
+                if ($PSItem -gt 0) {
+                    $true
+                } else {
+                    throw "Value must be greater than zero."
+                }
+            })]
+            [Parameter(Position = 1, Mandatory = $true)]
+            [int32]$Sides
+        )
+        $Results = New-Object "System.Collections.Generic.List[int32]"
+        $Rolls = 1..$Dice
+        $Rolls.ForEach({
+            $Results.Add($($this.DieRoll($Sides)))
+        })
+        $Statistics = $Results | Measure-Object -Sum -Average -Maximum -Minimum -StandardDeviation
+        $Statistics.Property = $MyInvocation | Select-Object -ExpandProperty Line
+        $Statistics | Add-Member -MemberType NoteProperty -Name DiceRolls -Value $Results
+        $Statistics | Get-Member | Where-Object -Property MemberType -eq Property | Select-Object -ExpandProperty Name | ForEach-Object { Write-Verbose "$PSItem = $($Statistics.$PSItem)" }
+        $Report = $Statistics | Select-Object -Property DiceRolls,Count,Average,Sum,Maximum,Minimum,StandardDeviation,@{Label = 'Request';Expression = { $PSItem.Property }},@{Label = 'StandardNotation';Expression = {"$($Dice)d$($Sides)"}}
+        Write-Output $Report
+    }
+
+$BagOfDice | Add-Member `
+    -MemberType ScriptMethod `
+    -Name FudgeDie `
+    -Value {
+        [CmdletBinding()]
+        param()
+        $Range = -1..1
+        Write-Output $($Range | Get-Random)
+    }
+
+$BagOfDice | Add-Member `
+    -MemberType ScriptMethod `
+    -Name FudgeRoll `
+    -Value {
+        [CmdletBinding()]
+        param(
+            [ValidateScript({
+                if ($PSItem -gt 0) {
+                    $true
+                } else {
+                    throw "Value must be greater than zero."
+                }
+            })]
+            [Parameter(Position = 0, Mandatory = $true)]
+            [int32]$Dice
+        )
+        $Results = New-Object "System.Collections.Generic.List[int32]"
+        $Rolls = 1..$Dice
+        $Rolls.ForEach({
+            $Results.Add($($this.FudgeDie()))
+        })
+        $Statistics = $Results | Measure-Object -Sum -Average -Maximum -Minimum -StandardDeviation
+        $Statistics.Property = $MyInvocation | Select-Object -ExpandProperty Line
+        $Statistics | Add-Member -MemberType NoteProperty -Name DiceRolls -Value $Results
+        $Statistics | Get-Member | Where-Object -Property MemberType -eq Property | Select-Object -ExpandProperty Name | ForEach-Object { Write-Verbose "$PSItem = $($Statistics.$PSItem)" }
+        $Report = $Statistics | Select-Object -Property DiceRolls,Count,Average,Sum,Maximum,Minimum,StandardDeviation,@{Label = 'Request';Expression = { $PSItem.Property }},@{Label = 'StandardNotation';Expression = {"$($Dice)d$($Sides)"}}
+        Write-Output $Report
+    }
+
+$BagOfDice | Add-Member `
+    -MemberType ScriptMethod `
+    -Name Percentile `
+    -Value {
+        [CmdletBinding()]
+        param()
+        $DiceRolled = @{
+            Tens = [int32]$($this.DieRoll(10))
+            Ones = [int32]$($this.DieRoll(10))
+        }
+        if($DiceRolled.Tens -gt 9){$DiceRolled.Tens = 0}
+        if($DiceRolled.Ones -gt 9){$DiceRolled.Ones = 0}
+        # Write-Host $DiceRolled
+        $Result = @([string]$($DiceRolled.Tens),[string]$($DiceRolled.Ones)) -join ''
+        Write-Output $Result
+    }
+
+$BagOfDice | Add-Member `
+    -MemberType ScriptMethod `
+    -Name CoinFlip `
+    -Value {
+        [CmdletBinding()]
+        param()
+        $Result = @('Heads','Tails') | Get-Random
+        Write-Output $Result
+    }
